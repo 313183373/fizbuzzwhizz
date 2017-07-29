9d856b38705295d4021da34aa451ddcb06ef2871
313183373 <313183373@qq.com>
2017-07-29 17:26:18 +0800

    code and test finished

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-function fizbuzzwhizz(a, b, c){
-  // write your code here...
+function fizbuzzwhizz(a, b, c, d){
+  let ans=-1;
+  let ab=a*b;
+  let ac=a*c;
+  let bc=b*c;
+  let abc=a*b*c;
+  let i=d;
+  if(i.toString().includes(a)){
+      ans="Fizz";
+  }else{
+      if(i%abc===0){
+          ans="FizzBuzzWhizz";
+      }else if(i%ab===0){
+          ans="FizzBuzz";
+      }else if(i%ac===0){
+          ans="FizzWhizz";
+      }else if(i%bc===0){
+          ans="BuzzWhizz";
+      }else if(i%a===0){
+          ans="Fizz";
+      }else if(i%b===0){
+          ans="Buzz";
+      }else if(i%c===0){
+          ans="Whizz";
+      }else{
+          ans=i;
+      }
+  }
+  return ans;
 }
--- src/test/main-test.js
+++ src/test/main-test.js
@@ -1,9 +1,115 @@
 'use strict';
-
 describe('fizbuzzwhizz', () => {
 
   // write your tests here...
-  it('should do something when some condition', () => {
-    expect(false).toEqual(true);
+  it('normal a', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=6;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Fizz");
+  });
+  it('b', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=20;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Buzz");
+  });
+  it('c', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=14;
+    let d=28;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Whizz");
+  });
+  it('special a is ab the same time', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=30;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Fizz");
+  });
+  it('normal ab', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=45;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("FizzBuzz");
+  });
+  it('normal ac', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=21;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("FizzWhizz");
+  });
+  it('special a is ac the same time', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=63;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Fizz");
+  });
+  it('normal bc', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=140;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("BuzzWhizz");
+  });
+  it('normal abc', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=105;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("FizzBuzzWhizz");
+  });
+  it('special a is abc the same time', () => {
+    //GIVEN
+    let a=3;
+    let b=5;
+    let c=7;
+    let d=315;
+    //WHEN
+    let result=fizbuzzwhizz(a,b,c,d);
+    //THEN
+    expect(result).toEqual("Fizz");
   });
 });
